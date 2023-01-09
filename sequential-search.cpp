e6d488a7fd20809eef4246ff3e90bb4b01154d37
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2023-01-09 14:10:06 -0700

    Create sequential-search.cpp

---
+++ sequential-search.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int location;
+    bool found = false;
+    location = 0;
+    int IDs[] = { 234, 567, 321, 567, 234, 789 };
+
+    int searchItem;
+    cout << "enter ID you are searching force:" << endl;
+    cin >> searchItem;
+
+    while (location < sizeof(IDs) / sizeof(IDs[0]) && !found) {
+        if (IDs[location] == searchItem)
+            found = true;
+        else
+            location++;
+    }
+    if (found == true)
+        cout << "ID " << searchItem << " was found in slot " << location << "." << endl;
+    else
+        cout << "ID was not found" << endl;
+}
